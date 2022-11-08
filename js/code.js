319cfced6400f12fc53ed0fd41e43abe49692e26
SIPHAMANDLAMLANJANA <114500540+Siphamandla201@users.noreply.github.com>
2022-11-08 13:11:29 +0200

    1st update

--- js/code.js
+++ js/code.js
@@ -1,11 +1,19 @@
-let fName = prompt("Name");
-let lName = prompt("Surname");
-let salary = prompt("Salary");
-let bonus = 500;
-let finalSalary = eval(`${salary}+${bonus}`);
+// let fName = prompt("Name");
+// let lName = prompt("Surname");
+// let salary = prompt("Salary");
+// let bonus = 500;
+// let finalSalary = eval(`${salary}+${bonus}`);
 
-document.write(`
-    Name: ${fName} <br> 
-    Surname:  ${lName} <br>
-    Salary: ${finalSalary}    
-`)
+// document.write(`
+//     Name: ${fName} <br> 
+//     Surname:  ${lName} <br>
+//     Salary: ${finalSalary}    
+// `)
+
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
